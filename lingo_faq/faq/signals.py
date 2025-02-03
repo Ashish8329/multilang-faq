655be2161b49dc87e7efdf0425d6a50fa4c0ccf3
ashish2302 <ashishauti123@gmail.com>
2025-02-03 12:54:35 +0530

    feat : config signals on faq creation.

---
+++ lingo_faq/faq/signals.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from faq.models import FAQ
+
+
+@receiver(post_save, sender=FAQ)
+def create_faq_translation(sender, instance, created, **kwargs):
+    """
+    Using djngo signals to create translations for the FAQ model
+    """
+    if created:
+        instance.translations = {
+            instance.language: {
+                "question": instance.question,
+                "answer": instance.answer,
+            }
+        }
+        instance.save()
