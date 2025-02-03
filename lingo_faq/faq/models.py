0de9ad9306b4093e7dea30372f7a57ff5171470a
ashish2302 <ashishauti123@gmail.com>
2025-02-03 11:06:45 +0530

    feat(faq): add FAQ model with enum choices and admin customization
    - Set up base enum choices
    - Added specific enum choices for FAQ model
    - Customized admin panel for better management

--- lingo_faq/faq/models.py
+++ lingo_faq/faq/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from tinymce.models import HTMLField
 
-# Create your models here.
+from base.base_models import BaseModel
+from faq.choices import LanguageChoices
+
+
+class FAQ(BaseModel):
+    question = models.TextField(
+        max_length=255,
+        verbose_name=_("Question"),
+        help_text=_("Enter the question"),
+    )
+    answer = HTMLField(
+        verbose_name=_("Answer"),
+        help_text=_("Enter the answer"),
+    )
+    language = models.CharField(
+        max_length=2,
+        choices=[x.value for x in LanguageChoices],
+        verbose_name=_("Language"),
+        help_text=LanguageChoices.get_help_text(),
+    )
+
+    class Meta:
+        verbose_name = _("FAQ")
+        verbose_name_plural = _("FAQs")
